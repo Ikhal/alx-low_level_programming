250dd946271c7896d3e866756e9756d41d65b57c
Ibrahim Suleiman <Ismuhammadmi7@gmail.com>
2022-06-30 07:38:57 -0700

    hot fixz

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,9 @@
-#!/bin/bash
+# ! / bin / bash
 #include <stdio.h>
-int main(void){
 
-	puts("\"Programming is like building a multilingual puzzle");
-	retun (0);
+int main(void)
+{
+
+puts("\"Programming is like building a multilingual puzzle");
+retun (0);
 }
